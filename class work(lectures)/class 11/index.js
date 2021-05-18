0c8ef84b1eb157631f756b8ca01889fbfb993000
naima-shk <naimashaikh550@gmail.com>
2021-05-19 01:11:32 +0500

    'started-working-on-basic-JS'

---
+++ class work(lectures)/class 11/index.js
@@ -0,0 +1,14 @@
+// 1) Access input  HTML element ---query selector
+//
+let inputElement =document.querySelector('input');
+console.log(inputElement);
+
+//get value of HTML element
+let inputValue= inputElement.value;
+console.log(inputValue);
+
+//access <h1> element
+let headingElement = document.querySelector('h1');
+console.log(headingElement);
+
+//change content of <h1> element
