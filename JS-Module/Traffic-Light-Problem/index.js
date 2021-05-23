92c172e223dd492ecb0c6787ac32aad47098fac1
naima-shk <naimashaikh550@gmail.com>
2021-05-23 14:50:34 +0500

    'traffic-light-problem'

--- JS-Module/Traffic-Light-Problem/index.js
+++ JS-Module/Traffic-Light-Problem/index.js
@@ -1,6 +1,8 @@
 function Traffic(){
-    let color=prompt('What is the color?');
-    if (color =='red') {
+    //let color=
+    prompt('What is the color?');
+
+    if (color == 'red') {
         return alert('this is red traffic light');
     } 
     else if (color == 'yellow'){
@@ -10,6 +12,6 @@ function Traffic(){
       return   alert('this is blue traffic light');
     }  
     else {
-        return  alert('this is black traffic light');
+      return  alert('this is black traffic light');
     }
 }
