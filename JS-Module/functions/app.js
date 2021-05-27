7db9f2d2b7a4a0e65fa58c49717766014d1379f9
naima-shk <naimashaikh550@gmail.com>
2021-05-27 20:02:32 +0500

    'array-implemented'

---
+++ JS-Module/functions/app.js
@@ -0,0 +1,20 @@
+function myArea(base,height){
+    let triangle= (base * height)/2;
+    return triangle;
+}
+//myArea(3,6);
+
+
+
+
+/* 
+Write a function that takes the base and height of a triangle and return its area. Don’t forget to return the result.The area of a triangle is: (base * height) / 2
+
+*/
+//ungraded coding challenges
+/*for (let i=0; i<=100; i++){
+  console.log(i);
+} */
+/*for (let i=1; i<=100; i++){
+  console.log('naima');
+} */
