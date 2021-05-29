1f078ebdbe0790269ca8d300de51c182d4981082
naima-shk <naimashaikh550@gmail.com>
2021-05-29 15:28:51 +0500

    'array-traversing-using-for-loop'

---
+++ JS-Module/loops/app.js
@@ -0,0 +1,6 @@
+
+//array traversing using for loop in ascending and descending order.
+let fruits =['apple','orange', 'mango'];
+for (let i=fruits.length-1; i>=0; i--){
+    console.log(fruits[i]);
+}
