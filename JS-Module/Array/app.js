2a8f2ad6a04618c9e48ce70e4147b28cca121933
naima-shk <naimashaikh550@gmail.com>
2021-06-03 14:51:33 +0500

    'sorting-implemented'

--- JS-Module/Array/app.js
+++ JS-Module/Array/app.js
@@ -1,6 +1,17 @@
- function isArray(){
+ /*function isArray(){
     let arr = ['Apple', 0,1,2,3,4];
     for (let i=0; i<arr.length; i++){
     console.log(arr[i]);
      }
     }
+*/
+    //sorting an array
+    const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// expected output: Array [1, 100000, 21, 30, 4]
