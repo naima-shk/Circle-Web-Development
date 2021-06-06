956b7f919d833b4fd3628481837b564266a3c3a7
naima-shk <naimashaikh550@gmail.com>
2021-06-06 13:54:57 +0500

    'Dom-css'

--- JS-Module/DOM/app.js
+++ JS-Module/DOM/app.js
@@ -1,8 +1,6 @@
+function myButton() {
+  document.querySelector("button").style.backgroundColor = "yellow";
+  let element = document.querySelector("button").style.border;
 
-function myButton(){
-document.querySelector('button').style.backgroundColor = "yellow";
-let element = document.querySelector('button').style.border;
-
-
-console.log(element);
+  console.log(element);
 }
